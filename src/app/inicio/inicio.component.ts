b0445be8e60b642f0b8df5c26dbe27af1248058f
xennis Rodriguez <xennislrj25@gmail.com>
2024-07-13 16:38:48 -0500

    no 404

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,23 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ElementRef, ViewChild} from '@angular/core';
 import { Router } from '@angular/router';
-import { LastRouteService } from '../last-route.service';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent implements OnInit {
-  constructor(
-    private router: Router,
-    private lastRouteService: LastRouteService
-  ) {}
-
-  ngOnInit(): void {
-    const lastRoute = this.lastRouteService.getLastRoute();
-    if (lastRoute === '/inicio') {
-      this.router.navigate(['/verifi']);
-    }
-    this.lastRouteService.setLastRoute('/inicio');
-  }
-}
+export class InicioComponent { }  
